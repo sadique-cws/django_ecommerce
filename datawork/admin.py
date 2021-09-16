f483ee04ec04f58b4eda073e4467d4f6216c43eb
Mujahid Alam <mujahidalam1997@gmail.com>
2021-09-16 16:00:50 +0530

    second

--- datawork/admin.py
+++ datawork/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from datawork.models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Address)
+admin.site.register(OrderItem)
--- datawork/views.py
+++ datawork/views.py
@@ -1,5 +1,13 @@
+from datawork.models import Category, Item
 from django.shortcuts import render
 
 # Create your views here.
 def homepages(req):
-    return render(req,"home.html")
+    return render(req,"home.html",{
+        'categories':Category.objects.all(),
+        'products':Item.objects.all(),
+        
+
+    })
+
+  
