bebc118aedb8f3704a042e0116b338ae2354bea4
sadique-cws <sadique.hussain96@gmail.com>
2021-09-16 15:59:05 +0530

    4th day

--- datawork/admin.py
+++ datawork/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from datawork.models import *
+
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Brand)
+admin.site.register(OrderItem)
+admin.site.register(Address)
+admin.site.register(Coupon)
+admin.site.register(Order)
--- datawork/views.py
+++ datawork/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from datawork.models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def homepages(req):
-    return render(req,"home.html")
+    return render(req,"home.html",{
+        'categories':Category.objects.all(),
+        'products' : Item.objects.all()
+    })
+
+def view_product(req,id,slug):
+    return render(req,"product.html",{
+        'product': Item.objects.get(pk=id),
+        'related_products':Item.objects.exclude(pk=id),
+    })
+
+def search(req):
+    return  render(req,"home.html",{
+        'categories': Category.objects.all(),
+        'products': Item.objects.filter(title__contains=req.GET.get('search'))
+    })
+
+@login_required()
+def add_to_cart(req,id):
+    pass
+
+
+def category(req,cat_slug):
+    return render(req,"home.html",{
+        'categories': Category.objects.all(),
+        'products': Item.objects.filter(category__slug=cat_slug)
+    })
