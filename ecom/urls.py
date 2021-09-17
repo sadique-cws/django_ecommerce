1ea478cc2147252ac091dc0a87dedad9c90d3b68
AMRIT UTSAV <lbrock513@gmail.com>
2021-09-17 16:04:49 +0530

    new commit

--- ecom/urls.py
+++ ecom/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepages,name="home"),
