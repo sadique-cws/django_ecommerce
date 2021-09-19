8eac75734f4066311db93922684a95f61ea261f8
AMRIT UTSAV <lbrock513@gmail.com>
2021-09-19 23:25:04 +0530

    new commit for login design by Amrit

--- ecom/settings.py
+++ ecom/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "datawork",
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -141,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ="/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
