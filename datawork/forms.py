ca06e5e16d29052944727a15f3256f13800cb0f6
AMRIT UTSAV <lbrock513@gmail.com>
2021-09-21 16:05:41 +0530

    project done

---
+++ datawork/forms.py
@@ -0,0 +1,7 @@
+from django import forms 
+from datawork.models import *
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        exclude = ("user",)
--- datawork/views.py
+++ datawork/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.db.models import Q
 from django.utils import timezone
 from datawork.models import *
+from datawork.forms import *
+import random
+
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def homepage(req):
@@ -19,6 +22,15 @@ def view_product(req,id,slug):
         'product': Item.objects.get(pk=id),
         'related_products':Item.objects.exclude(pk=id),
     })
+    
+
+def removeCoupon(req):
+    order = Order.objects.filter(user=req.user,ordered=False).first()
+    order.coupon = None
+    order.save()
+    return redirect(cart)
+
+
 
 def addCoupon(req):
     if req.method == "POST":
@@ -42,6 +54,10 @@ def cart(req):
     return render(req,"order_summary.html",{
         'pro' : Order.objects.filter(ordered=False, user=req.user).first()
     })
+def myOrder(req):
+    return render(req,"my_order.html",{
+        'order' : Order.objects.filter(ordered=True, user=req.user)
+    })
 def search(req):
     return  render(req,"home.html",{
         'categories': Category.objects.all(),
@@ -59,7 +75,6 @@ def addToCart(req,id):
     order = Order.objects.filter(user=req.user,ordered=False)
     if(order.exists()):
         order = order[0]
-
         if order.item.filter(item__id=id).exists():
             order_item.qty += 1
             order_item.save()
@@ -91,6 +106,7 @@ def removeFromCart(req,id):
                 messages.success(req,"product qty updated successfully")
             else:
                 order.item.remove(order_item)
+                order_item.delete()
                 messages.success(req,"product removed successfully")
         else:
             return redirect(cart)
@@ -104,6 +120,66 @@ def removeFromCart(req,id):
         messages.success(req,"added in cart successfully")
     return redirect(cart)
             # if not exist
+
+
+@login_required()
+def singleOrderItemRemove(req,id):
+    product  = get_object_or_404(Item,pk=id)
+
+    order = Order.objects.filter(user=req.user,ordered=False).first()
+    
+    if(order):
+        order_item = OrderItem.objects.filter(item=product,ordered=False,user=req.user)[0]
+        order.item.remove(order_item)
+        order_item.delete()
+
+
+    return redirect(cart)
+
+
+def checkout(req):
+    form = AddressForm(req.POST or None,use_required_attribute=False)
+    order = Order.objects.filter(user=req.user,ordered=False).first()
+    if req.method == "POST":
+        addressId = req.POST.get("saveAddressId",None)
+        if addressId:
+            address = Address.objects.get(id=addressId)
+            order.address = address
+            order.save()
+            return redirect(payment)
+        else:
+            if form.is_valid():
+                a = form.save(commit=False)
+                a.user = req.user
+                a.save()
+                order.address = a
+                order.save()
+                return redirect(payment)
+            else:
+                return redirect(checkout)
+
+    return render(req,"checkout.html",{
+        'form':form,
+        'address': Address.objects.filter(user=req.user)
+    })
+
+def makeOrder(req):
+    order = Order.objects.filter(user=req.user,ordered=False).first()
+    random_string = "ORBR0000" + str(random.randint(10000, 99999)) + "IN"
+    order.ref_code = random_string
+    order.ordered = True
+
+    for oi in order.item.all():
+        oi.ordered = True
+        oi.save()
+    order.save()
+    return True       
+
+
+
+def payment(req):
+    if makeOrder(req):
+        return render(req,"payment.html")
 
 def category(req,cat_slug):
     return render(req,"home.html",{
